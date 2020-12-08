5bc683e0c7dd3b2e34d823caa0a458325f09625f
ruchim24 <ruchimajoka24@gmail.com>
2020-12-09 01:24:13 +0530

    modified signup screen added validations

---
+++ app/src/main/java/com/example/myapp/Common/LoginSignup/SignUp3rdClass.java
@@ -0,0 +1,16 @@
+package com.example.myapp.Common.LoginSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapp.R;
+
+public class SignUp3rdClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up3rd_class);
+    }
+}
